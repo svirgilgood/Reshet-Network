4bd07fd78e518734bc9d8d6ccfe8db1dbba8b249
e2dubba <svirgilgoodwin@gmail.com>
2025-05-04 07:59:30 -0600

    initial commit for the update queries, but needs major updating

--- codex/src/db/query_creator.rs
+++ codex/src/db/query_creator.rs
@@ -1,16 +1,23 @@
 extern crate handlebars;
 
-use handlebars::Handlebars;
+use handlebars::{Handlebars, RenderError, TemplateError};
 // use serde_json::json;
 use std::collections::HashMap;
-use sophia::graph::{*, inmem::FastGraph};
+//use sophia::graph::{*, inmem::FastGraph};
 // use utils;
-
 
 // pub fn store_to_graph_pattern() {
 
 // }
+//
 
+//enum Err {
+//    TemplateError(TemplateError),
+//    RenderError(RenderError),
+//}
+
+//impl From<TemplateError> for Err {}
+//impl From<RenderError> for Err {}
 
 /// Create an update query from add, delete and graph pattern options.
 /// All three of these variables are optional, (but with out an add or delete, nothing will happen with the resulting query)
@@ -28,71 +35,83 @@ use sophia::graph::{*, inmem::FastGraph};
 /// assert_eq!(query, "\nDELETE {\n  <d> <e> <f> .\n}\nINSERT {\n  <a> <b> <c> .\n}\nWHERE {\n  \n}".to_string())
 /// ```
 ///
-pub fn update_query(add: Option<&str>, delete: Option<&str>, graph_pattern: Option<&str>) -> String {
-  let mut handlebars = Handlebars::new();
-  handlebars.register_template_file("insert", "./src/db/queries/insert.sparql").unwrap();
+pub fn update_query(
+    add: Option<&str>,
+    delete: Option<&str>,
+    graph_pattern: Option<&str>,
+) -> Result<String, TemplateError> {
+    let mut handlebars = Handlebars::new();
+    handlebars.register_template_file("insert", "./src/db/queries/insert.sparql")?;
 
-  let mut replacement_dic  = HashMap::new();
-  if let Some(value) = add {
-    replacement_dic.insert("insert_triples".to_string(), value.to_owned());
-  }
-  if let Some(value) = delete {
-    replacement_dic.insert("delete_triples".to_string(), value.to_owned());
-  }
-  if let Some(value) = graph_pattern {
-    replacement_dic.insert("graph_pattern".to_string(), value.to_owned());
-  }
+    let mut replacement_dic = HashMap::new();
+    if let Some(value) = add {
+        replacement_dic.insert("insert_triples".to_string(), value.to_owned());
+    }
+    if let Some(value) = delete {
+        replacement_dic.insert("delete_triples".to_string(), value.to_owned());
+    }
+    if let Some(value) = graph_pattern {
+        replacement_dic.insert("graph_pattern".to_string(), value.to_owned());
+    }
 
-  let res = handlebars.render("insert", &replacement_dic).unwrap();
-  return res;
+    let res = handlebars.render("insert", &replacement_dic)?;
+    return Ok(res);
 }
 
 #[cfg(test)]
 mod tests {
-  use sophia::ns::Namespace;
-use sophia::serializer::TripleSerializer;
-use sophia::serializer::nt::NtSerializer;
-use sophia::serializer::Stringifier;
+    use sophia::ns::Namespace;
+    use sophia::serializer::nt::NtSerializer;
+    use sophia::serializer::Stringifier;
+    use sophia::serializer::TripleSerializer;
 
+    use super::*;
 
-use super::*;
+    // Testing Sophia
+    #[test]
+    fn create_graph_vec() {
+        let mut graph: FastGraph = FastGraph::new();
+        let ex = Namespace::new("https://example.com/").unwrap();
+        graph
+            .insert(
+                &ex.get("Bob").unwrap(),
+                &ex.get("knows").unwrap(),
+                &ex.get("Alice").unwrap(),
+            )
+            .unwrap();
 
-  // Testing Sophia
-  #[test]
-  fn create_graph_vec() {
-    let mut graph: FastGraph = FastGraph::new();
-    let ex = Namespace::new("https://example.com/").unwrap();
-    graph.insert(&ex.get("Bob").unwrap(), &ex.get("knows").unwrap(), &ex.get("Alice").unwrap()).unwrap();
+        let result = graph.triples();
+        let mut nt_string = NtSerializer::new_stringifier();
+        let example = nt_string.serialize_triples(result).unwrap().as_str();
+        assert_eq!(
+            example,
+            "<https://example.com/Bob> <https://example.com/knows> <https://example.com/Alice>.\n"
+        );
+    }
 
-    let result = graph.triples();
-    let mut nt_string = NtSerializer::new_stringifier();
-    let example = nt_string.serialize_triples(result).unwrap().as_str();
-    assert_eq!(example, "<https://example.com/Bob> <https://example.com/knows> <https://example.com/Alice>.\n");
+    // testing query insertion
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 
-  }
+    #[test]
+    fn expect_insert_with_add() {
+        let add = Some("hello");
+        let ret = update_query(add, None, None);
+        let expected = "\nDELETE {\n  \n}\nINSERT {\n  hello\n}\nWHERE {\n  \n}".to_string();
+        assert_eq!(ret, expected);
+    }
 
-
-  // testing query insertion
-  #[test]
-  fn it_works() {
-    assert_eq!( 2 + 2, 4);
-  }
-
-  #[test]
-  fn expect_insert_with_add() {
-    let add = Some("hello");
-    let ret = update_query(add, None, None);
-    let expected = "\nDELETE {\n  \n}\nINSERT {\n  hello\n}\nWHERE {\n  \n}".to_string();
-    assert_eq!(ret, expected);
-  }
-
-  #[test]
-  fn expect_all_three() {
-    let add = Some("hello");
-    let delete = Some("world");
-    let graph_pattern = Some("!");
-    let ret = update_query(add, delete, graph_pattern);
-    assert_eq!(ret, "\nDELETE {\n  world\n}\nINSERT {\n  hello\n}\nWHERE {\n  !\n}".to_string());
-  }
-
+    #[test]
+    fn expect_all_three() {
+        let add = Some("hello");
+        let delete = Some("world");
+        let graph_pattern = Some("!");
+        let ret = update_query(add, delete, graph_pattern);
+        assert_eq!(
+            ret,
+            "\nDELETE {\n  world\n}\nINSERT {\n  hello\n}\nWHERE {\n  !\n}".to_string()
+        );
+    }
 }
